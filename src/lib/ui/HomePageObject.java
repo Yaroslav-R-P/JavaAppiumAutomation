a2853f101f0d6cf31e505194a4ac083bb76a88aa
Yaroslav-R-P <75783575+Yaroslav-R-P@users.noreply.github.com>
2024-07-17 18:01:47 +0300

    Jp/hw EX11 (#16)
    * L7 EX11 выполнение ДЗ ЕХ11. Рефактор теста на свайпы онбординга и проверку тайтлов

--- src/lib/ui/HomePageObject.java
+++ src/lib/ui/HomePageObject.java
@@ -3,12 +3,12 @@ package lib.ui;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
-public class HomePageObject extends MainPageObject {
+abstract public class HomePageObject extends MainPageObject {
     public HomePageObject(AppiumDriver driver) {
         super(driver);
     }
-    private static final String
-            MAIN_TOOLBAR = "id:org.wikipedia:id/main_toolbar";
+    protected static String
+            MAIN_TOOLBAR;
 
     public void isMainPageDisplayed() {
         this.waitForElementPresent(MAIN_TOOLBAR, "Cannot find MAIN_TOOLBAR, home page is not displayed", 10);
